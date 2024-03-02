c5edc925efc0b52dcf646750fcef507bbb1a001a
Retina Kundu <retinakundu94@gmail.com>
2024-03-02 00:37:30 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-function addingEventListener() {
-}
+// index.js
+(function() {
+    function addingEventListener() {
+      const input = document.getElementById('button');
+  
+      function clickAlert() {
+        alert('I was clicked!');
+      }
+  
+      input.addEventListener('click', clickAlert);
+    }
+  
+    if (typeof module !== 'undefined' && module.exports) {
+      module.exports = addingEventListener; // Export the function for CommonJS environments
+    } else {
+      window.addingEventListener = addingEventListener; // Expose the function globally for browsers
+      addingEventListener(); // Call the function immediately in a browser environment
+    }
+  
+  })();
+  
